2a36b376096198dc1f321e5ec29c0cdb67edce33
Febriqgal Purnama <febriqgalp@gmail.com>
2023-07-06 14:42:17 +0700

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
-import Fp from "../../public/logotrans.svg";
+import Fp from "../../public/febproject.svg";
+import Link from "next/link";
 export default function FooterC() {
   return (
-    <footer className="flex flex-col items-center justify-center space-x-0">
-      <h1 className="text-xs font-light">Created by Febriqgal🔥</h1>
-      <Image height={40} className="mt-4" src={Fp} alt="#" />
+    <footer className="flex flex-col items-center justify-center pt-4">
+      <h1 className="text-xs font-light">From</h1>
+      <Link target="_blank" href={"https://www.instagram.com/febproject_/"}>
+        <Image height={40} src={Fp} alt="#" />
+      </Link>
     </footer>
   );
 }
